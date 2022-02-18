fe29b9ca61998bca026f34a4332614a1f3e3c0b2
hassan.alijani@contech.university <hassan.alijani@contech.university>
2022-02-17 20:45:47 -0500

    input & exception handlig

--- InputValueAndExceptionHandling.py
+++ InputValueAndExceptionHandling.py
@@ -5,5 +5,6 @@ except ValueError:
     print("Exception while converting to integer!")	
 except Exception as e:
 	print("Exception error: " + str(e))
-
+else:
+    print("The result is correct if all operations are successful!")
 	
